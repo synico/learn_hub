0cc6f16b7f0685cdb35dccacc1615c1eb6f8d25b
synico <synico@gmail.com>
2016-06-12 11:16:28 +0800

    initial commit

---
+++ python/http_get.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+import httplib
+import urllib
+
+params = urllib.urlencode({'@LOOKUPADDRESS':'12.9.138.11'})
+headers = {"Content-type":"application/x-www-form-urlencoded","Accept":"text/plain","User-Agent":"synico@gmail.com"}
+headers1 = {'User-Agent':'synico@gmail.com'}
+
+conn = httplib.HTTPConnection('bot.whatismyipaddress.com')
+conn.debuglevel = 1
+conn.request('POST', '/', params, headers1)
+r1 = conn.getresponse()
+#print r1.status, r1.reason
+
+data1 = r1.read()
+print data1
+conn.close()
+
+#conn1 = httplib.HTTPConnection('bot.whatismyipaddress.com')
+#conn1.debuglevel = 1
+#conn1.request("GET","/ip/12.9.138.11")
+#r2 = conn1.getresponse()
+#data2 = r2.read()
+#print data2
