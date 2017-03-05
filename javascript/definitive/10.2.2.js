5b9a7e556f3c3fefc567d34b65ca36b44e3b95ac
Nick <synico@gmail.com>
2017-03-05 18:06:47 +0800

    code snippet of javascript definitive guide

---
+++ javascript/definitive/10.2.2.js
@@ -0,0 +1,19 @@
+var com;
+if (!com) com = {};
+if (!com.davidflanagan) com.davidflanagan = {};
+com.davidflanagan.Class = {};
+
+(function() {
+	function define(data) { counter++; }
+	function provides(o, c) {}
+	
+	var counter = 0;
+	
+	function getCounter() { return counter; }
+	
+	var ns = com.davidflanagan.Class;
+	ns.define = define;
+	ns.provides = provides;
+	ns.getCounter = getCounter;
+	console.log("end...");
+}) ();
