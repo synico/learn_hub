b0d3ffbcbe540fb99b21a16d3a0abfd5f517089e
Nick <synico@gmail.com>
2017-03-05 18:54:38 +0800

    update 10.2.2.js

--- javascript/definitive/10.2.2.js
+++ javascript/definitive/10.2.2.js
@@ -4,16 +4,16 @@ if (!com.davidflanagan) com.davidflanagan = {};
 com.davidflanagan.Class = {};
 
 (function() {
-	function define(data) { counter++; }
-	function provides(o, c) {}
-	
-	var counter = 0;
-	
-	function getCounter() { return counter; }
-	
-	var ns = com.davidflanagan.Class;
-	ns.define = define;
-	ns.provides = provides;
-	ns.getCounter = getCounter;
-	console.log("end...");
+    function define(data) { counter++; }
+    function provides(o, c) {}
+
+    var counter = 0;
+
+    function getCounter() { return counter; }
+
+    var ns = com.davidflanagan.Class;
+    ns.define = define;
+    ns.provides = provides;
+    ns.getCounter = getCounter;
+    console.log("end...");
 }) ();
