426d4cc0a1ad6a088fc2a6bf3fc98395d7c42c11
synico <synico@gmail.com>
2017-03-12 19:36:47 +0800

    update for function/method

---
+++ javascript/thegoodpart/chapter4.js
@@ -0,0 +1,22 @@
+var add = function (a, b) {
+    return a + b;
+}
+
+var myObj = {
+    value: 0,
+    increment: function (inc) {
+        this.value += typeof inc === 'number' ? inc : 1;
+    }
+}
+myObj.double = function () {
+    var that = this;
+    var helper = function () {
+        that.value = add(that.value, that.value);
+        console.log("value in helper(): " + that.value);
+    };
+
+    helper();
+}
+
+myObj.double();
+console.log(myObj.value);
