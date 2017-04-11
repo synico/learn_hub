135ee6d8997472a06b56fe316c6fecf53d24e197
Nick Liu <synico@gamil.com>
2017-04-11 11:13:04 +0800

    add new java project for practising data structure

---
+++ java/data_structure/src/com/ds/tree/BinarySearchTree.java
@@ -0,0 +1,137 @@
+package com.ds.tree;
+
+import java.util.Comparator;
+
+@SuppressWarnings("all")
+public class BinarySearchTree<T extends Comparable<? super T>> {
+	
+	private static class BinaryNode<E> {
+		
+		E element;
+		
+		BinaryNode<E> left;
+		
+		BinaryNode<E> right;
+		
+		BinaryNode(E theElement) {
+			this(theElement, null, null);
+		}
+		
+		BinaryNode(E theElement, BinaryNode<E> lt, BinaryNode<E> rt) {
+			element = theElement;
+			left = lt;
+			right = rt;
+		}
+	}
+		
+	private BinaryNode<T> root;
+	private Comparator<? super T> cmp;
+	
+	public BinarySearchTree() {
+		this(null);
+	}
+	
+	public BinarySearchTree(Comparator<? super T> c) {
+		root = null;
+		cmp = c;
+	}
+	
+	private int myCompare(T lhs, T rhs) {
+		if(cmp != null) {
+			return cmp.compare(lhs, rhs);
+		} else {
+			return ((Comparable)lhs).compareTo(rhs);
+		}
+	}
+	
+	public void makeEmpty() {
+		root = null;
+	}
+	
+	public boolean isEmpty() {
+		return root == null;
+	}
+	
+	public boolean contains(T x) {
+		return contains(x, root);
+	}
+	
+	public T findMin() {
+		if(isEmpty())
+			throw new NullPointerException();
+		return findMin(root).element;
+	}
+	
+	public T findMax() {
+		if(isEmpty()) 
+			throw new NullPointerException();
+		return findMax(root).element;
+	}
+	
+	public void insert(T x) {
+		root = insert(x, root);
+	}
+	
+	public void remove(T x) {
+		root = remove(x, root);
+	}
+	
+	public void printTree() {
+		
+	}
+	
+	private boolean contains(T x, BinaryNode<T> t) {
+		if(t == null) {
+			return false;
+		}
+		
+		int compareResult = this.myCompare(x, t.element);
+		
+		if(compareResult < 0) {
+			return contains(x, t.left);
+		} else if(compareResult > 0) {
+			return contains(x, t.right);
+		} else {
+			return true;
+		}
+	}
+	
+	private BinaryNode<T> findMin(BinaryNode<T> t) {
+		if(t == null) {
+			return null;
+		} else if(t.left == null) {
+			return t;
+		}
+		return findMin(t);
+	}
+	
+	private BinaryNode<T> findMax(BinaryNode<T> t) {
+		if(t != null) {
+			while(t.right != null) {
+				t = t.right;
+			}
+		} 
+		return t;
+	}
+	
+	private BinaryNode<T> insert(T x, BinaryNode<T> t) {
+		if(t == null) {
+			t = new BinaryNode(x, null, null);
+		} 
+		
+		int compareResult = this.myCompare(x, t.element);
+		if(compareResult < 0) {
+			
+		}
+		return null;
+	}
+	
+	private BinaryNode<T> remove(T x, BinaryNode<T> t) {
+		return null;
+	}
+	
+	private void printTree(BinaryNode<T> t) {
+		
+	}
+
+}
