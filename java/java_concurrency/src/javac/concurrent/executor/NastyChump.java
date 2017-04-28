d543106d2d228fede7578d3de0e3161575d42dba
Nick Liu <synico@gamil.com>
2017-04-28 14:51:52 +0800

    update executor

--- java/java_concurrency/src/javac/concurrent/executor/NastyChump.java
+++ java/java_concurrency/src/javac/concurrent/executor/NastyChump.java
@@ -6,16 +6,18 @@ import java.util.concurrent.TimeUnit;
 public class NastyChump {
 
 	public static void main(String[] args) throws InterruptedException {
-		Socket[] sockets = new Socket[1000];
+		Socket[] sockets = new Socket[400];
 		for (int i=0; i < sockets.length; i++) {
 			try {
-				sockets[i] = new Socket("localhost", 80);
+				sockets[i] = new Socket("localhost", 8080);
 				System.out.println(i);
+//				TimeUnit.SECONDS.sleep(2);
+				sockets[i].close();
 			} catch (Exception ex) {
 				ex.printStackTrace();
 			}
 		}
-		TimeUnit.DAYS.sleep(1);
+		TimeUnit.SECONDS.sleep(120);
 	}
 
 }
--- java/java_concurrency/src/javac/concurrent/executor/TaskExecutionWebServer.java
+++ java/java_concurrency/src/javac/concurrent/executor/TaskExecutionWebServer.java
@@ -1,18 +1,26 @@
 package javac.concurrent.executor;
 
+/**
+ * http://www.javaspecialists.eu/tutorials/001-Hacking-together-our-own-ArrayList-in-less-than-10-minutes.html
+ */
+
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class TaskExecutionWebServer {
 	
-	private static final int NTHREADS = 1000;
-	private static final Executor exec = Executors.newFixedThreadPool(NTHREADS);
+	private static final int NTHREADS = 10;
+//	private static final Executor exec = Executors.newFixedThreadPool(NTHREADS);
+	private static final Executor exec = Executors.newCachedThreadPool();
+	private static final AtomicInteger counter = new AtomicInteger(0);
 	
 	public static void handleRequest(Socket conn) {
+		System.out.println("Connection from " + conn);
 		try(
 			InputStream in = conn.getInputStream();
 			OutputStream out = conn.getOutputStream()
@@ -33,7 +41,8 @@ public class TaskExecutionWebServer {
 
 	public static void main(String[] args) {
 		while(true) {
-			try(ServerSocket socket = new ServerSocket(80)) {
+			System.out.println("received: " + counter.incrementAndGet());
+			try(ServerSocket socket = new ServerSocket(8080)) {
 				final Socket connection = socket.accept();
 				exec.execute(()->handleRequest(connection));
 			} catch(Exception ex) {
