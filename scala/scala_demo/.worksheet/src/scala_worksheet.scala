8634e660ec12690bbf47791270a323de431a4fff
synico <synico@gmail.com>
2017-05-24 19:26:01 +0800

    remove unused files

--- scala/scala_demo/.worksheet/src/scala_worksheet.scala
+++
@@ -1,3 +0,0 @@
-object scala_worksheet {;import org.scalaide.worksheet.runtime.library.WorksheetSupport._; def main(args: Array[String])=$execute{;$skip(68); 
-  println("Welcome to the Scala worksheet")}
-}
