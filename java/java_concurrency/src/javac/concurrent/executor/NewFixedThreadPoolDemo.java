179494538ace7f929461a67f83ef89aa92ee17ad
synico <synico@gmail.com>
2017-08-24 19:55:25 +0800

    daily update

--- java/java_concurrency/src/javac/concurrent/executor/NewFixedThreadPoolDemo.java
+++ java/java_concurrency/src/javac/concurrent/executor/NewFixedThreadPoolDemo.java
@@ -1,10 +1,36 @@
 package javac.concurrent.executor;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
 public class NewFixedThreadPoolDemo {
+	
+	private ExecutorService exec = Executors.newFixedThreadPool(5);
+	
+	public void runBatchJobs() {
+		List<LoadDataTask> jobs = new ArrayList<LoadDataTask>();
+		for(int i=0; i < 100; i++) {
+			jobs.add(new LoadDataTask(i));
+		}
+		List<Future<String>> ret;
+		try {
+			ret = exec.invokeAll(jobs);
+			for(Future<String> job : ret) {
+				if(job.isDone()) {
+					System.out.println(job.get());
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		exec.shutdown();
+	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		new NewFixedThreadPoolDemo().runBatchJobs();
 	}
 
 }
