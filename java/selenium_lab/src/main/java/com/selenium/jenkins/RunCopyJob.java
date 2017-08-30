16e53c232746e425e81214e78675e64d371ed19c
Nick Liu <synico@gamil.com>
2017-08-30 16:54:57 +0800

    create a new project for selenium

---
+++ java/selenium_lab/src/main/java/com/selenium/jenkins/RunCopyJob.java
@@ -0,0 +1,43 @@
+package com.selenium.jenkins;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+
+public class RunCopyJob {
+	
+	private static final String JENKINS_URL = "http://jenkins-em.avnet.com:8080/login";
+	private static final Log log = LogFactory.getLog(RunCopyJob.class);
+	
+	private WebDriver webDriver = new HtmlUnitDriver();
+	
+	private boolean login2Jenkins() {
+		boolean rst = false;
+		webDriver.get(JENKINS_URL);
+		log.info(webDriver.getPageSource());
+		//Enter username
+		WebElement usernameField = webDriver.findElement(By.id("j_username"));
+		usernameField.sendKeys("nick.liu");
+		//Enter password
+		WebElement passwordField = webDriver.findElement(By.name("j_password"));
+		passwordField.sendKeys("August2017");
+		//Checked remember me checkbox
+		WebElement rememberMeField = webDriver.findElement(By.id("remember_me"));
+		rememberMeField.clear();
+		//Submit login form
+		WebElement submitField = webDriver.findElement(By.name("Submit"));
+		submitField.submit();
+		
+		log.info(webDriver.findElement(By.xpath("//*[@id='job_RM_DX_Copy']/td[3]/a")));
+		return rst;
+	}
+	
+	public static void main(String[] args) {
+		RunCopyJob cpJob = new RunCopyJob();
+		cpJob.login2Jenkins();
+	}
+
+}
