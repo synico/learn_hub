99630bdbc3306c5c11ebb265c5041394d4be49a8
synico <synico@gmail.com>
2017-09-02 18:32:58 +0800

    update doc

--- java/java_concurrency/src/javac/concurrent/queue/blocking/SynchronousQueueDemo.java
+++ java/java_concurrency/src/javac/concurrent/queue/blocking/SynchronousQueueDemo.java
@@ -1,7 +1,6 @@
 package javac.concurrent.queue.blocking;
 
 import java.util.concurrent.BlockingQueue;
-import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.SynchronousQueue;
 import java.util.concurrent.TimeUnit;
 
@@ -9,26 +8,25 @@ public class SynchronousQueueDemo {
 	
 	private static final BlockingQueue<String> syncQueue = new SynchronousQueue<String>(); 
 	
-	private static final CountDownLatch latch = new CountDownLatch(1);
-	
 	class Consumer implements Runnable {
 
 		@Override
 		public void run() {
 			for(;;) {
+				String rst = null;
+				//option1
 				try {
-					latch.await();
-				} catch (InterruptedException e1) {
-					e1.printStackTrace();
+					rst = syncQueue.poll(1, TimeUnit.SECONDS);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
 				}
-				String rst = syncQueue.poll();
-				if(rst == null) {
-					try {
-						TimeUnit.MILLISECONDS.sleep(100);
-					} catch (InterruptedException e) {
-						e.printStackTrace();
-					}
-				} else {
+				//option2
+//				try {
+//					rst = syncQueue.take();
+//				} catch (InterruptedException e) {
+//					e.printStackTrace();
+//				}
+				if(rst != null) {
 					System.out.println(rst);
 				}
 			}
@@ -41,10 +39,18 @@ public class SynchronousQueueDemo {
 		@Override
 		public void run() {
 			for(;;) {
-				latch.countDown();
-				if(syncQueue.offer("test: " + System.currentTimeMillis())) {
-					return;
+				//option1 
+				try {
+					syncQueue.offer("test: " + System.currentTimeMillis(), 1, TimeUnit.SECONDS);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
 				}
+				//option2
+//				try {
+//					syncQueue.put("test: " + System.currentTimeMillis());
+//				} catch (InterruptedException e) {
+//					e.printStackTrace();
+//				}
 			}
 		}
 		
@@ -52,13 +58,10 @@ public class SynchronousQueueDemo {
 	
 	public void test() {
 		Consumer con = new Consumer();
-		
 		Producer pro = new Producer();
 		
-		Thread proT = new Thread(pro);
-		Thread conT = new Thread(con);
-		conT.start();
-		proT.start();
+		new Thread(pro).start();
+		new Thread(con).start();;
 	}
 
 	public static void main(String[] args) {
