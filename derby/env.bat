bd5a37f3aec59731c649f5c5e482bf68644b1f36
Nick Liu <synico@gamil.com>
2017-09-11 13:28:39 +0800

    update

--- derby/env.bat
+++ derby/env.bat
@@ -1,18 +1,31 @@
 @echo off
 
-SET JAVA_HOME=D:\java\7.0
+if "%~1" == "" (
+    SET JDK_VERSION=8.0
+) else (
+    SET JDK_VERSION=%~1
+)
 
-SET ANT_HOME=E:\ant\1.9.7
+SET JAVA_HOME=D:\java\%JDK_VERSION%
 
-SET MAVEN_HOME=E:\maven\3.2.3
+SET SCALA_HOME=D:\scala\2.11
 
-SET DERBY_HOME=E:\derby\10.3.3.0
+SET SBT_HOME=D:\scala\sbt\1.0.11
 
-SET PATH=%PATH%;%JAVA_HOME%\bin;%ANT_HOME%\bin;%MAVEN_HOME%\bin;%DERBY_HOME%\bin;
+SET MAVEN_HOME=D:\maven\3.3.9
 
+SET GRADLE_HOME=D:\gradle\3.5
+
+SET DERBY_HOME=D:\derby\10.3.3.0
+
+SET ANT_HOME=E:\apache-ant-1.9.7
+SET IVY_HOME=E:\apache-ivy-2.4.0
+
+SET GIT_HOME=D:\Git
+
+SET PATH=%JAVA_HOME%\bin;%MAVEN_HOME%\bin;%GRADLE_HOME%\bin;%DERBY_HOME%\bin;%ANT_HOME%\bin;%SCALA_HOME%\bin;%SBT_HOME%\bin;%GIT_HOME%\cmd;
 
 REM set classpath
 SET CLASSPATH=%DERBY_HOME%\lib\derby.jar;%DERBY_HOME%\lib\derbytools.jar;
 
-REM derby net url: 'jdbc:derby://localhost:1527/D:\IBM\WCDE_ENT70\db\mall_network;user=APP;password=APP;create=true';
-REM 'jdbc:derby:D:\IBM\WCDE_ENT70\db\mall_network;create=true'
+REM derby net url: 'jdbc:derby://localhost:1527/D:\IBM\WCDE_ENT70\db\mall_network;user=root;password=APP;create=true';
