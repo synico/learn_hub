052f84256ec05d3088870386ec282abad7e7b3c8
Nick Liu <synico@gamil.com>
2017-09-20 17:03:59 +0800

    add demo

--- java/java_concurrency/src/javac/map/LinkedHashMapDemo.java
+++ java/java_concurrency/src/javac/map/LinkedHashMapDemo.java
@@ -1,14 +1,37 @@
 package javac.map;
 
+import java.util.ArrayList;
 import java.util.LinkedHashMap;
+import java.util.List;
 import java.util.Map;
 
 public class LinkedHashMapDemo {
 	
 	private static volatile Map<String, String> map = new LinkedHashMap<String, String>();
+	
+	private List list;
+	
+	private List initList() {
+		list = new ArrayList();
+		List temp = new ArrayList();
+		Map<String, String> map = new LinkedHashMap<String, String>();
+		map.put("1", "one");
+		map.put("2", "two");
+		map.put("3", "three");
+		temp.add(map);
+		list.add(temp);
+		return list;
+	}
+	
+	private void traverseList() {
+		// Need pay attention to the lifecycle of generic type 
+		List<Map<String, String>> theList = initList();
+		System.out.println(theList);
+	}
 
 	public static void main(String[] args) {
 		map.put("a", "A");
+		new LinkedHashMapDemo().traverseList();
 	}
 
 }
