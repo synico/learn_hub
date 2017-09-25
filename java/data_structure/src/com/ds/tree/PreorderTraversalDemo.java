ea11e96853199a39252094e60dee6b34d0af5dc5
Nick Liu <synico@gamil.com>
2017-09-25 16:46:24 +0800

    chapter4(tree) of ds&aa in java

---
+++ java/data_structure/src/com/ds/tree/PreorderTraversalDemo.java
@@ -0,0 +1,43 @@
+package com.ds.tree;
+
+import java.io.File;
+
+@SuppressWarnings("all")
+public class PreorderTraversalDemo extends File {
+	
+	public PreorderTraversalDemo(String name) {
+		super(name);
+	}
+	
+	public void printName(int depth) {
+		for(int i = 0; i < depth; i++) {
+			System.out.print("----");
+		}
+//		System.out.println(getName() + " | " + super.getAbsolutePath());
+		System.out.println(getName() + "|" + depth);
+	}
+	
+	public void listAll() {
+		listAll(0);
+	}
+	
+	private void listAll(int depth) {
+		printName(depth);
+		
+		if(isDirectory()) {
+			String [] entries = list();
+			for(int i = 0; i < entries.length; i++) {
+				PreorderTraversalDemo child = new PreorderTraversalDemo(getPath() + separatorChar + entries[i]);
+				child.listAll(depth + 1);
+			}
+			
+		}
+	}
+	
+	public static void main(String args[]) {
+		PreorderTraversalDemo demo = new PreorderTraversalDemo("./src");
+		demo.listAll();
+	}
+
+
+}
