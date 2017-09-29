c0dd2a0521d88afa035672c4ff2ae4b3ae5e9d8d
Nick Liu <synico@gmail.com>
2017-09-29 15:07:57 +0800

    update document 20170929

---
+++ java/java_concurrency/src/javac/generic/CalcMaxSizeOfArrayDeque.java
@@ -0,0 +1,20 @@
+package javac.generic;
+
+import java.util.concurrent.TimeUnit;
+
+public class CalcMaxSizeOfArrayDeque {
+
+	public static void main(String[] args) {
+		int size = 1;
+		while(true) {
+			try {
+				TimeUnit.MILLISECONDS.sleep(10);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			size = size << 1;
+			System.out.println(size);
+		}
+	}
+
+}
