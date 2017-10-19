4ef21df071c801a71a28fec7982c7a2e1b9885b4
Nick Liu <synico@gmail.com>
2017-10-19 12:42:25 +0800

    update workspace

---
+++ java/data_structure/src/PropertiesUtil.java
@@ -0,0 +1,43 @@
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FilenameFilter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+import java.util.Properties;
+
+public class PropertiesUtil {
+	
+	private static List<InputStream> loadFiles(String path) {
+		File filePath = new File(path);
+		File zhFiles[] = filePath.listFiles(new FilenameFilter() {
+			public boolean accept(File dir, String name) {
+				boolean isAccept = false;
+				if(name.contains("zh_CN")) {
+					isAccept = true;
+				}
+				return isAccept;
+			}
+		});
+		for(File file : zhFiles) {
+//			System.out.println(file.getName());
+			try(InputStream input = new FileInputStream(file)) {
+				Properties p = new Properties();
+				p.load(input);
+				System.out.println(file.getName() + " : " + p.size());
+				p.forEach((key, value) -> System.out.println(key + " = " + value));
+			} catch (FileNotFoundException e) {
+				e.printStackTrace();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		return null;
+	}
+
+	public static void main(String[] args) {
+		PropertiesUtil.loadFiles("D:/IBM/WCDE_ENT70/workspace/Stores/src/AvnetSAS");
+	}
+
+}
