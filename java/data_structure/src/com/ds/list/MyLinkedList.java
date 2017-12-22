0ca474d48c5cc65f3564ab03fe8ef5f63be377a7
Nick Liu <synico@gmail.com>
2017-12-22 17:03:44 +0800

    one way list reverse

--- java/data_structure/src/com/ds/list/MyLinkedList.java
+++ java/data_structure/src/com/ds/list/MyLinkedList.java
@@ -2,51 +2,51 @@ package com.ds.list;
 
 import java.util.Iterator;
 
-public class MyLinkedList<E> implements Iterable<E>{
-	
-	private Node<E> head;
-	
-	private Node<E> tail;
-	
-	private int size;
-	
-	private static class Node<E> {
-		private E data;
-		private Node<E> prev;
-		private Node<E> next;
-		
-		Node(Node<E> prev, E data, Node<E> next) {
-			this.prev = prev;
-			this.data = data;
-			this.next = next;
-		}
-	}
-	
-	private class LinkedListIterator implements Iterator<E>{
-		
-		private Node<E> current = head.next;
+public class MyLinkedList<E> implements Iterable<E> {
 
-		@Override
-		public boolean hasNext() {
-			return (current != tail);
-		}
+    private Node<E> head;
 
-		@Override
-		public E next() {
-			return current.data;
-		}
-	}
-	
-	public Iterator<E> iterator() {
-		return new LinkedListIterator();
-	}
-	
-	public int size() {
-		return this.size;
-	}
-	
-	public boolean isEmpty() {
-		return (size() == 0);
-	}
+    private Node<E> tail;
+
+    private int size;
+
+    private static class Node<E> {
+        private E data;
+        private Node<E> prev;
+        private Node<E> next;
+
+        Node(Node<E> prev, E data, Node<E> next) {
+            this.prev = prev;
+            this.data = data;
+            this.next = next;
+        }
+    }
+
+    private class LinkedListIterator implements Iterator<E> {
+
+        private Node<E> current = head.next;
+
+        @Override
+        public boolean hasNext() {
+            return (current != tail);
+        }
+
+        @Override
+        public E next() {
+            return current.data;
+        }
+    }
+
+    public Iterator<E> iterator() {
+        return new LinkedListIterator();
+    }
+
+    public int size() {
+        return this.size;
+    }
+
+    public boolean isEmpty() {
+        return (size() == 0);
+    }
 
 }
--- java/data_structure/src/com/ds/list/OneWayLinkedList.java
+++ java/data_structure/src/com/ds/list/OneWayLinkedList.java
@@ -1,93 +1,136 @@
 package com.ds.list;
 
 public class OneWayLinkedList<E> {
-	
-	private Node<E> head;
-	
-	private Node<E> tail;
-	
-	private int size = 0;
-	
-	public boolean add(E e) {
-		final Node<E> last = new Node<E>(e, null);
-		if (head == null) {
-			head = last;
-			tail = last;
-		} else {
-			tail.next = last;
-			tail = last;
-		}
-		size++;
-		return true;		
-	}
-	
-	public boolean remove(E e) {
-		Node<E> cursor = head;
-		for (int idx = 0; idx < size; idx++) {
-			if(e != null && e.equals(cursor.next.value)) {
-				cursor.next = cursor.next.next;
-				size--;
-				return true;
-			}
-		}
-		return false;
-	}
-	
-	public void reverse() {
-		Node<E> cursor = null;
-		Node<E> previous = null;
-		Node<E> current = head;
-		for (int idx = 0; idx < size; idx++) {
-			cursor = current.next;
-			current.next = previous;
-			previous = current;
-			current = cursor;
-		}
-		current = tail;
-		tail = head;
-		head = current;
-	}
-	
-	public int size() {
-		return size;
-	}
-	
-	public void printList(String msg) {
-		System.out.print(msg);
-		if (head == null) {
-			System.out.println("List is empty");
-		} else {
-			Node<E> cursor = head;
-			for (int idx = 0; idx < size; idx++) {
-				System.out.print("[ (" + cursor.value + ") | -> ]");
-				cursor = cursor.next;
-			}
-		}
-		System.out.println();
-	}
-	
-	private static class Node<E> {
-		E value;
-		Node<E> next;
-		
-		Node(E value, Node<E> next) {
-			this.value = value;
-			this.next = next;
-		}
-	}
 
-	public static void main(String[] args) {
-		OneWayLinkedList<Integer> owll = new OneWayLinkedList<Integer>();
-		owll.add(1);
-		owll.add(2);
-		owll.add(3);
-		owll.printList("Print entire list: ");
-		owll.remove(2);
-		owll.printList("Print list after remove(2): ");
-		owll.add(4);
-		owll.add(5);
-		owll.reverse();
-		owll.printList("Print list after reversed: ");
-	}
+    private Node<E> head;
+
+    private Node<E> tail;
+
+    private int size = 0;
+    
+    private static class Node<E> {
+        E value;
+        Node<E> next;
+
+        Node(E value, Node<E> next) {
+            this.value = value;
+            this.next = next;
+        }
+        
+        public String toString() {
+            return "Node Value: " + value;
+        }
+    }
+
+    public boolean add(E e) {
+        final Node<E> last = new Node<E>(e, null);
+        if (head == null) {
+            head = last;
+            tail = last;
+        } else {
+            tail.next = last;
+            tail = last;
+        }
+        size++;
+        return true;
+    }
+
+    public boolean remove(E e) {
+        Node<E> cursor = head;
+        for (int idx = 0; idx < size; idx++) {
+            if (e != null && e.equals(cursor.next.value)) {
+                cursor.next = cursor.next.next;
+                size--;
+                return true;
+            }
+        }
+        return false;
+    }
+    
+    /** VERSION1 without parameter Node<E> **/
+
+    public void reverse() {
+        Node<E> cursor = null;
+        Node<E> previous = null;
+        Node<E> current = head;
+        for (int idx = 0; idx < size; idx++) {
+            cursor = current.next;
+            current.next = previous;
+            previous = current;
+            current = cursor;
+        }
+        current = tail;
+        tail = head;
+        head = current;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public void printList1(String msg) {
+        System.out.print(msg);
+        if (head == null) {
+            System.out.println("List is empty");
+        } else {
+            Node<E> cursor = head;
+            for (int idx = 0; idx < size; idx++) {
+                System.out.print("[ (" + cursor.value + ") | -> ]");
+                cursor = cursor.next;
+            }
+        }
+        System.out.println();
+    }
+    
+    public void printList2(String msg) {
+        System.out.print(msg);
+        for (Node<E> cursor = tail;;) {
+            System.out.print("[ (" + cursor.value + ") | -> ]");
+            cursor = cursor.next;
+            if (cursor == null) {
+                break;
+            }
+        }
+        System.out.println();
+    }
+
+    /** VERSION2 input parameter Node<E> **/
+    public Node<E> previousNode(Node<E> head) {
+        Node<E> newHead = null;
+        System.out.println("value: " + head.value);
+        if (head.next == null) {
+            return head;
+        } 
+        newHead = previousNode(head.next);
+        head.next.next = head;
+        head.next = null;
+        return newHead;
+    }
+    
+    public int fib(int i) {
+        if(i <= 1) {
+            return 1;
+        } else {
+            return i + fib(i-1);
+        }
+    }
+    
+
+    public static void main(String[] args) {
+        OneWayLinkedList<Integer> owll = new OneWayLinkedList<Integer>();
+        owll.add(1);
+        owll.add(2);
+        owll.add(3);
+//        owll.printList1("Print entire list: ");
+//        owll.remove(2);
+//        owll.printList1("Print list after remove(2): ");
+        owll.add(4);
+        owll.add(5);
+//        owll.reverse();
+//        owll.printList1("Print list after reversed: ");
+        owll.printList1("Foreach list before reverse: ");
+        owll.head = owll.previousNode(owll.head);
+        owll.printList1("Foreach list after reverse: ");
+    }
 
 }
