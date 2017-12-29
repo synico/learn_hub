0d1b4aa6366d7c44916838266ab8bc85f780fcee
Nick Liu <synico@gmail.com>
2017-12-29 14:09:54 +0800

    update insert sort

--- java/data_structure/src/com/ds/sorting/ShellSort.java
+++ java/data_structure/src/com/ds/sorting/ShellSort.java
@@ -22,12 +22,29 @@ public class ShellSort {
     private static void psg(String msg) {
         System.out.println(msg);
     }
+    
+    public static void shellSorting(int [] a) {
+        int delta;//define delta
+        for(delta = a.length/2; delta > 0; delta = delta/2) {
+            //start to loop list
+            for(int p = delta; p < a.length ; p++) {
+                int temp = a[p];
+                int i;
+                for(i = p; i > delta -1  && temp < a[i-delta] ;i = i -delta) {
+                    a[i] = a[i - delta];
+                }
+                a[i] = temp;
+            }
+        }
+    }
 
     public static void main(String[] args) {
         int[] demo = {81, 94, 11, 96, 12, 35, 17, 95, 28, 58, 41, 75, 15};
         PrintUtil.printList(demo);
-        int[] sortedDemo = ShellSort.shellSort1(demo);
-        PrintUtil.printList(sortedDemo);
+//        int[] sortedDemo = ShellSort.shellSort1(demo);
+//        PrintUtil.printList(sortedDemo);
+        ShellSort.shellSorting(demo);
+        PrintUtil.printList(demo);
     }
 
 }
