8433bbf79c6a4428013ab73d2f83bd867f2f6091
Nick Liu <synico@gmail.com>
2017-12-29 12:39:12 +0800

    add new tasks for Jan 2018 - algorithm

--- java/data_structure/src/com/ds/sorting/InsertionSort.java
+++ java/data_structure/src/com/ds/sorting/InsertionSort.java
@@ -26,7 +26,7 @@ public class InsertionSort {
         for (int p = 1; p < array.length; p++) {
             int temp = array[p];
             for (j = p; j > 0; j--) {
-                if(temp < array[j - 1]) {
+                if (temp < array[j - 1]) {
                     array[j] = array[j - 1];
                     array[j - 1] = temp;
                 }
@@ -34,27 +34,61 @@ public class InsertionSort {
         }
         return array;
     }
-    
+
     public static int[] insertionSort3(int[] array) {
         int j;
-        for(int p = 1; p < array.length; p++) {
-            int temp = array[p];//Element need to be checked from array[0] to array[p]
-            for(j = p; j> 0 && temp < array[j - 1] ; j--) {
+        for (int p = 1; p < array.length; p++) {
+            int temp = array[p];// Element need to be checked from array[0] to array[p]
+            for (j = p; j > 0 && temp < array[j - 1]; j--) {
                 array[j] = array[j - 1];
             }
             array[j] = temp;
         }
         return array;
     }
-    
+
     public static int[] insertionSort4(int[] array) {
         int j;
-        for(int p = 1; p < array.length; p++) {
+        for (int p = 1; p < array.length; p++) {
             int temp = array[p];
             j = p;
-            while(j > 0 && temp < array[j - 1]) {
+            while (j > 0 && temp < array[j - 1]) {
                 array[j] = array[j - 1];
                 j--;
+            }
+            array[j] = temp;
+        }
+        return array;
+    }
+
+    public static int[] insertionSort5(int[] array) {
+        for (int i = 1; i < array.length; i++) {
+            int temp = array[i];
+            int j;
+            for (j = i; j > 0 && temp < array[j - 1]; j--) {
+                array[j] = array[j - 1];
+                System.out.println(j);
+            }
+            array[j] = temp;
+        }
+
+        return array;
+    }
+
+    public static int[] binaryInsertSort(int[] array) {
+        for (int p = 1; p < array.length; p++) {
+            int temp = array[p];
+            int low = 0, high = p, j = 0;
+            while (low <= high) {
+                j = (low + high) / 2;
+                if (temp < array[j]) {
+                    high = j - 1;
+                } else {
+                    low = j + 1;
+                }
+            }
+            for(int i = p; i > j; i--) {
+                array[i] = array[i - 1];
             }
             array[j] = temp;
         }
@@ -64,7 +98,7 @@ public class InsertionSort {
     public static void main(String[] args) {
         int[] demo = { 34, 8, 64, 51, 32, 21 };
         PrintUtil.printList(demo);
-        int[] sortedArray = insertionSort4(demo);
+        int[] sortedArray = binaryInsertSort(demo);
         PrintUtil.printList(sortedArray);
     }
 
