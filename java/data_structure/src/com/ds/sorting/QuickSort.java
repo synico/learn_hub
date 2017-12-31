c38ebe716bf0201d07a79a528b9297ac36baf087
synico <synico@gmail.com>
2017-12-31 23:44:29 +0800

    update 20171231

--- java/data_structure/src/com/ds/sorting/QuickSort.java
+++ java/data_structure/src/com/ds/sorting/QuickSort.java
@@ -4,12 +4,27 @@ import com.ds.util.PrintUtil;
 
 public class QuickSort {
     
+    public static void bubbleSort(int [] a) {
+        for(int i = 0; i < a.length; i++) {
+            int temp;
+            for(int j = 0; j < a.length - i - 1; j++) {
+                if(a[j] > a[j + 1]) {
+                    temp = a[j];
+                    a[j] = a[j + 1];
+                    a[j + 1] = temp;
+                }
+            }
+        }
+    }
+    
     public static void quickSort(int [] a) {
+        
     }
 
     public static void main(String[] args) {
         int[] demo = {13, 81, 92, 43, 65, 31, 57, 26, 75, 0};
         PrintUtil.printList(demo);
+        QuickSort.quickSort(demo);
         PrintUtil.printList(demo);
     }
 
