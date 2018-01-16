6f885aeaf6d33633d27760ff4e169ae7ed7032ac
synico <synico@gmail.com>
2018-01-16 20:55:18 +0800

    update 20180116

--- java/data_structure/src/com/ds/tree/AVLTree.java
+++ java/data_structure/src/com/ds/tree/AVLTree.java
@@ -2,7 +2,7 @@ package com.ds.tree;
 
 public class AVLTree {
     
-    private static class AVLNode<T extends Comparable<?>> {
+    class AVLNode<T extends Comparable<?>> {
         T element;
         AVLNode<T> left;
         AVLNode<T> right;
@@ -18,23 +18,35 @@ public class AVLTree {
             this.right = rightNode;
             this.height = 0;
         }
-    }
-    
-    //recursive method
-    public static int triangle(int n) {
-        System.out.println("Entering: n = " + n);
-        if(n == 1) {
-            System.out.println("Returning 1");
-            return 1;
-        } else {
-            int temp = n + triangle(n - 1);
-            System.out.println("Returning " + temp);
-            return temp;
+        
+        public String toString() {
+            return "" + element;
         }
     }
+    
+    public <T extends Comparable<?>> AVLNode<T> insert(T val, AVLNode<T> root) {
+        if(root == null) {
+            root = new AVLNode<T>(val);
+        } else {
+            if(this.myCompareTo(val, root.element) < 0) {
+                root.left = insert(val, root.left);
+            } else {
+                root.right = insert(val, root.right);
+            }
+        }
+        return root;
+    }
+    
+    private <T> int myCompareTo(T lv, T rv) {
+        return ((Comparable<T>)lv).compareTo(rv);
+    }
+
 
     public static void main(String[] args) {
-        AVLTree.triangle(5);
+        AVLTree avlTree = new AVLTree();
+        AVLNode<Integer> root = avlTree.insert(1, null);
+        avlTree.insert(2, root);
+        System.out.println(avlTree);
     }
 
 }
