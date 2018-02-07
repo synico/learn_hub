e70cc538660cb35c8e1dd6b493872e7448d727dd
Nick Liu <synico@gmail.com>
2018-02-07 16:54:17 +0800

    update D20180207

---
+++ java/renewtech/src/main/java/com/effjava/chapter2/Builder.java
@@ -0,0 +1,9 @@
+package com.effjava.chapter2;
+
+public interface Builder<T> {
+    
+    String s = "x";
+
+    public T build();
+    
+}
---
+++ java/renewtech/src/main/java/com/effjava/chapter2/NutritionFacts.java
@@ -0,0 +1,12 @@
+package com.effjava.chapter2;
+
+public class NutritionFacts {
+    
+    private final int servingSize;
+    private final int servings;
+    
+    private NutritionFacts() {
+        
+    }
+    
+}
