c8073f0cb3fb71d57416a7b540c4f24387e0b97b
Nick Liu <synico@gmail.com>
2018-03-16 17:04:23 +0800

    kafka demo code update

--- kafka/kafka_demo/src/main/java/com/kafka/consumer/MessageListener.java
+++ kafka/kafka_demo/src/main/java/com/kafka/consumer/MessageListener.java
@@ -2,10 +2,12 @@ package com.kafka.consumer;
 
 import java.util.Collections;
 import java.util.Properties;
+import java.util.concurrent.TimeUnit;
 
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.TopicPartition;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -21,6 +23,7 @@ public class MessageListener {
         kafkaProps.put("group.id", "ConsumerGroup1");
         kafkaProps.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
         kafkaProps.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+        kafkaProps.put("client.id", "consumer");
         consumer = new KafkaConsumer<String, String>(kafkaProps);
     }
     
@@ -28,15 +31,21 @@ public class MessageListener {
         if(consumer == null) {
             setUpProducer();
         }
-        consumer.subscribe(Collections.singletonList("topic1"));
+//        consumer.subscribe(Collections.singletonList("topic1"));
+        //build topic partition for consumer
+        TopicPartition topicPartition = new TopicPartition("topic1", 3);
+        consumer.assign(Collections.singleton(topicPartition));
+        //poll messages by offset
+        consumer.seek(topicPartition, 0L);
         //loop
         try {
             while(true) {
                 ConsumerRecords<String, String> records = consumer.poll(100);
                 for(ConsumerRecord<String, String> record : records) {
-                    log.info("topic = %s, partition = %s, offset = %s, key = %s, message = %s\n", 
+//                    TimeUnit.SECONDS.sleep(10);
+                    log.info("\nMsg Listener1 ===> [ topic = {}, partition = {}, offset = {}, key = %s, message = {} ]\n", 
                             record.topic(), record.partition(), record.offset(), record.key(), record.value());
-                    System.out.println(record.topic() + ", " + record.partition() + ", " + record.offset() + ", " + record.key() + ", " + record.value());
+//                    System.out.println(record.topic() + ", " + record.partition() + ", " + record.offset() + ", " + record.key() + ", " + record.value());
                 }
             }
         } catch (Exception ex) {
--- kafka/kafka_demo/src/main/java/com/kafka/producer/MessageSender1.java
+++ kafka/kafka_demo/src/main/java/com/kafka/producer/MessageSender1.java
@@ -1,13 +1,19 @@
 package com.kafka.producer;
 
+import java.util.List;
 import java.util.Properties;
 
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.Producer;
 import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.kafka.clients.producer.RecordMetadata;
+import org.apache.kafka.common.PartitionInfo;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class MessageSender1 {
+    
+    private static final Logger log = LoggerFactory.getLogger(MessageSender1.class);
     
     private Properties kafkaProps = new Properties();
     private Producer<String, String> producer;
@@ -16,6 +22,7 @@ public class MessageSender1 {
         kafkaProps.put("bootstrap.servers", "192.168.1.219:9011,192.168.1.219:9012");
         kafkaProps.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
         kafkaProps.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        kafkaProps.put("client.id", "producer1");
         producer = new KafkaProducer<String, String>(kafkaProps);
     }
     
@@ -23,10 +30,17 @@ public class MessageSender1 {
         if(producer == null) {
             setUpProducer();
         }
-        ProducerRecord<String, String> record = new ProducerRecord<>("topic1", "Precision Products", "France");
+        List<PartitionInfo> partitions = producer.partitionsFor("topic1");
+        partitions.forEach(System.out::println);
+        
+        ProducerRecord<String, String> record = null;
         try {
-            RecordMetadata recordMeta = producer.send(record).get();
-            recordMeta.partition();
+            for(PartitionInfo partition : partitions) {
+                int partitionId = partition.partition();
+                record = new ProducerRecord<>("topic1", partitionId, "Message sender" + partitionId, "content" + System.currentTimeMillis());
+                RecordMetadata recordMeta = producer.send(record).get();
+                log.info("offset: {}", recordMeta.offset());
+            }
         } catch (Exception ex) {
             ex.printStackTrace();
         }
