25ccdc9ef6fb4786fc9682f8e026e07467c0c89a
Nick Liu <synico@gmail.com>
2018-04-12 16:17:30 +0800

    D20180412 setup hystrix demo

---
+++ java/data_structure/src/leetcode/array/RotateImage.java
@@ -0,0 +1,15 @@
+package leetcode.array;
+
+/** Medium **/
+public class RotateImage {
+    
+    public void rotate(int[][] matrix) {
+        
+    }
+
+    public static void main(String[] args) {
+        int[][] case1 = {{5, 1, 9, 11}, {2, 4, 8, 10}, {13, 3, 6, 7}, {15, 14, 12, 16}};
+        System.out.println(case1[0].length);
+    }
+
+}
