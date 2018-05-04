c3374523288be9bd44547aef78a8726ac51a9c60
Nick Liu <synico@gmail.com>
2018-05-04 16:11:58 +0800

    D20180504

--- java/springcloud_demo/eureka_service/src/main/java/org/eureka/service/HelloController.java
+++ java/springcloud_demo/eureka_service/src/main/java/org/eureka/service/HelloController.java
@@ -6,6 +6,7 @@ import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,8 +19,9 @@ public class HelloController {
     @Autowired
     private DiscoveryClient client;
     
-    @RequestMapping(value = "/hello", method = RequestMethod.GET)
-    public String sayHello() {
+    @RequestMapping(headers = "version=v2", value = "/hello", method = RequestMethod.GET)
+    public String sayHello(@RequestHeader("Accept") String accept) {
+        logger.info("Accept: " + accept);
         List<ServiceInstance> instances = client.getInstances("eureka-hello-service");
         for(ServiceInstance instance : instances) {
             logger.info("/hello, host:" + instance.getHost() + ", service_id:" + instance.getServiceId());
