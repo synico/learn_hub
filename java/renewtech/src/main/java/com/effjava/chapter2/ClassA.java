803b3999797b45e347cd9478ecf7626fa33a6077
Nick Liu <synico@gmail.com>
2018-05-08 16:19:50 +0800

    D20180508 interview question about super()

---
+++ java/renewtech/src/main/java/com/effjava/chapter2/ClassA.java
@@ -0,0 +1,15 @@
+package com.effjava.chapter2;
+
+public class ClassA {
+    
+    private int i = 0;
+    
+    public ClassA() {
+        
+    }
+    
+    public ClassA(int i) {
+        this.i = i;
+    }
+
+}
---
+++ java/renewtech/src/main/java/com/effjava/chapter2/ClassB.java
@@ -0,0 +1,11 @@
+package com.effjava.chapter2;
+
+public class ClassB extends ClassA {
+    
+    private int j;
+
+    public ClassB(int j) {
+        this.j = j;
+    }
+
+}
