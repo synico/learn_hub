5ad29ec2e5d700f88a3bd05aa8b5120251e3e44a
nick@p50 <synico@qq.com>
2018-06-13 22:17:02 +0800

    add batch git command in linux

---
+++ document/gitb.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo $1 $2 $3
+
+for sub in `ls`
+do
+  if [ -d $sub ]
+  then
+    echo $sub
+    cd $sub
+    git $1 $2 $3
+    cd ..
+    echo "------------------------------------------"
+  fi
+done
